e8ee80853295a14859b2c653e1b74aa60248b3e3
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-03 18:41:10 +0100

    setup TextInput component

---
+++ components/TextInput/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+interface TextInputProps {
+  label: string;
+  placeholder: string;
+  error?: boolean;
+}
+
+const TextInput = ({
+  error,
+  label,
+  placeholder,
+  ...props
+}: React.ComponentProps<"input"> & TextInputProps) => {
+  return (
+    <div className="flex w-full flex-col gap-y-(--sp-100)">
+      <div className="flex justify-between">
+        <label
+          htmlFor={props.name}
+          className="text-(length:--fs-14) leading-(--lh-120) text-(--clr-blue-950)"
+        >
+          {label}
+        </label>
+        {error && (
+          <span className="text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem] text-(--clr-red-500)">
+            This field is required
+          </span>
+        )}
+      </div>
+
+      <input
+        placeholder={placeholder}
+        className={`rounded-(--sp-100) border ${error ? "border-(--clr-red-500)" : "border-(--clr-purple-200)"} foucs:outline-(--clr-purple-600) px-(--sp-200) py-(--sp-100) text-(length:--fs-16) leading-(--lh-120) font-medium text-(--clr-blue-950) placeholder:text-(--clr-grey-500) focus:outline-1 active:bg-white`}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default TextInput;
