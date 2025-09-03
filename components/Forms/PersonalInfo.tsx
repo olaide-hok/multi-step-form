542566c78a9193fbf6786e9e69e68ccf6e515f43
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-03 18:40:40 +0100

    setup personal info form UI

---
+++ components/Forms/PersonalInfo.tsx
@@ -0,0 +1,39 @@
+import TextInput from "../TextInput";
+
+const PersonalInfo = () => {
+  return (
+    <div className="flex w-[21.4375rem] flex-col gap-y-(--sp-300) rounded-[0.625rem] bg-(--clr-white) px-(--sp-300) py-(--sp-400) shadow-[0_25px_40px_-20px_rgba(0,0,0,0.10)]">
+      <div className="flex flex-col gap-y-(--sp-100)">
+        <h2 className="text-(length:--fs-24) leading-(--lh-120) font-bold text-(--clr-blue-950)">
+          Personal info
+        </h2>
+        <p className="text-(length:--fs-16) leading-(--lh-120) text-(--clr-grey-500)">
+          Please provide your name, email address, and phone number.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-y-(--sp-300)">
+        <TextInput
+          name="name"
+          label="Name"
+          placeholder="e.g. Stephen King"
+          type="text"
+        />
+        <TextInput
+          name="email"
+          type="email"
+          label="Email Address"
+          placeholder="e.g. stephenking@lorem.com"
+        />
+        <TextInput
+          name="phoneNumber"
+          label="Phone Number"
+          placeholder="e.g. +1234567890"
+          type="text"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
