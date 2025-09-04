a2b67e1d40903836c242815d83e65cb16b42add4
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-04 19:17:35 +0100

    update Home page with personal info and progress sidebar comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
-import Image from 'next/image';
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+import PersonalInfo from "@/components/Forms/PersonalInfo";
+import ProgressSidebar from "@/components/Progress-Sidebar";
+import TextInput from "@/components/TextInput";
 
 export default function Home() {
-    return <div>Multi Step Form</div>;
+  return (
+    <div className="flex h-screen w-full flex-col justify-between md:justify-items-center">
+      <div className="relative flex w-full flex-col md:w-[42.875rem]">
+        <ProgressSidebar />
+        <div className="absolute top-[6.03rem] px-(--sp-200)">
+          <PersonalInfo />
+        </div>
+      </div>
+      {/* Go Back/Next Footer */}
+      <div className="flex justify-between bg-(--clr-white) p-(--sp-200)">
+        <Button
+          variant="tertiary"
+          name="Go Back"
+          className="hidden text-(length:--fs-14)"
+        />
+        <div></div>
+        <Button
+          variant="primary"
+          name="Next Step"
+          className="px-(--sp-200) py-[0.59375rem] text-(length:--fs-14)"
+        />
+      </div>
+    </div>
+  );
 }
