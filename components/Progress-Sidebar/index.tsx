79bbd840cd881228a081dc88959c171ec38b1e6f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-05 18:46:43 +0100

    refactor component to recieve props and use in jsx render.

--- components/Progress-Sidebar/index.tsx
+++ components/Progress-Sidebar/index.tsx
@@ -1,102 +1,42 @@
 "use client";
-import { useState } from "react";
 
-const ProgressSidebar = () => {
-  const [active, setActive] = useState(1);
+interface Steps {
+  id: number;
+  name: string;
+}
+
+interface ProgressSidebarProps {
+  steps: Steps[];
+  currentStep: number;
+}
+
+const ProgressSidebar = ({ steps, currentStep }: ProgressSidebarProps) => {
   const bgActive = "bg-(--clr-blue-200) text-(--clr-blue-950)";
 
-  const handleClick = (index: number) => {
-    setActive(index);
-  };
-
   return (
-    <section className="sidebar flex w-full flex-row items-start justify-center gap-x-(--sp-200) pt-(--sp-400) md:h-full md:flex-col md:justify-normal md:py-(--sp-500) lg:w-[17.125rem] lg:flex-col lg:gap-y-(--sp-400) lg:px-(--sp-400)">
-      <div className="flex items-center gap-x-(--sp-200)">
-        <button
-          type="button"
-          className={`${
-            active === 1
-              ? bgActive
-              : "border border-(--clr-white) text-(--clr-white)"
-          } h-[2.0625rem] w-[2.0625rem] cursor-pointer rounded-full text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem]`}
-          onClick={() => handleClick(1)}
-        >
-          1
-        </button>
-        <div className="hidden flex-col uppercase md:flex">
-          <span className="text-(length:--fs-12) leading-(--lh-120) text-(--clr-blue-300)">
-            Step 1
-          </span>
-          <span className="text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem] text-(--clr-white)">
-            Your info
-          </span>
-        </div>
-      </div>
-      <div className="flex items-center gap-x-(--sp-200)">
-        <button
-          type="button"
-          className={`${
-            active === 2
-              ? bgActive
-              : "border border-(--clr-white) text-(--clr-white)"
-          } h-[2.0625rem] w-[2.0625rem] cursor-pointer rounded-full text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem]`}
-          onClick={() => handleClick(2)}
-        >
-          2
-        </button>
-        <div className="hidden flex-col uppercase md:flex">
-          <span className="text-(length:--fs-12) leading-(--lh-120) text-(--clr-blue-300)">
-            Step 2
-          </span>
-          <span className="text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem] text-(--clr-white)">
-            select plan
-          </span>
-        </div>
-      </div>
-      <div className="flex items-center gap-x-(--sp-200)">
-        <button
-          type="button"
-          className={`${
-            active === 3
-              ? bgActive
-              : "border border-(--clr-white) text-(--clr-white)"
-          } h-[2.0625rem] w-[2.0625rem] cursor-pointer rounded-full text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem]`}
-          onClick={() => handleClick(3)}
-        >
-          3
-        </button>
-        <div className="hidden flex-col uppercase md:flex">
-          <span className="text-(length:--fs-12) leading-(--lh-120) text-(--clr-blue-300)">
-            Step 3
-          </span>
-          <span className="text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem] text-(--clr-white)">
-            Add-ons
-          </span>
-        </div>
-      </div>
-      <div className="flex items-center gap-x-(--sp-200)">
-        <button
-          type="button"
-          className={`${
-            active === 4
-              ? bgActive
-              : "border border-(--clr-white) text-(--clr-white)"
-          } h-[2.0625rem] w-[2.0625rem] cursor-pointer rounded-full text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem]`}
-          onClick={() => handleClick(4)}
-        >
-          4
-        </button>
-        <div className="hidden flex-col md:flex">
-          <div className="flex flex-col uppercase">
+    <section className="sidebar flex w-full flex-row items-start justify-center gap-x-(--sp-200) pt-(--sp-400) md:h-full md:flex-col md:justify-normal md:gap-y-(--sp-400) md:rounded-[0.625rem] md:px-[1.69rem] md:py-(--sp-500) lg:w-[17.125rem] lg:flex-col lg:px-(--sp-400)">
+      {steps.map((step) => (
+        <div key={step.id} className="flex items-center gap-x-(--sp-200)">
+          <button
+            type="button"
+            className={`${
+              currentStep + 1 === step.id
+                ? bgActive
+                : "border border-(--clr-white) text-(--clr-white)"
+            } h-[2.0625rem] w-[2.0625rem] cursor-pointer rounded-full text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem]`}
+          >
+            {step.id}
+          </button>
+          <div className="hidden flex-col uppercase md:flex">
             <span className="text-(length:--fs-12) leading-(--lh-120) text-(--clr-blue-300)">
-              Step 4
+              Step {step.id}
             </span>
             <span className="text-(length:--fs-14) leading-(--lh-120) font-bold tracking-[0.0625rem] text-(--clr-white)">
-              Summary
+              {step.name}
             </span>
           </div>
         </div>
-      </div>
+      ))}
     </section>
   );
 };
