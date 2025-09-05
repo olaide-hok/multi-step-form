c36f0d36e0c3d789ec421b0434ca9427c7dda616
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-05 18:45:51 +0100

    update tailwind classes

--- components/Forms/PersonalInfo.tsx
+++ components/Forms/PersonalInfo.tsx
@@ -2,9 +2,9 @@ import TextInput from "../TextInput";
 
 const PersonalInfo = () => {
   return (
-    <div className="flex w-[21.4375rem] flex-col gap-y-(--sp-300) rounded-[0.625rem] bg-(--clr-white) px-(--sp-300) py-(--sp-400) shadow-[0_25px_40px_-20px_rgba(0,0,0,0.10)]">
+    <div className="mx-(--sp-200) mb-[8.5rem] flex w-[21.4375rem] flex-col gap-y-(--sp-300) rounded-[0.625rem] bg-(--clr-white) px-(--sp-300) py-(--sp-400) shadow-[0_25px_40px_-20px_rgba(0,0,0,0.10)] md:m-0 md:w-full md:gap-y-(--sp-400) md:px-0 md:py-0 md:pt-[1.69rem] md:shadow-none lg:gap-y-(--sp-500)">
       <div className="flex flex-col gap-y-(--sp-100)">
-        <h2 className="text-(length:--fs-24) leading-(--lh-120) font-bold text-(--clr-blue-950)">
+        <h2 className="text-(length:--fs-24) leading-(--lh-120) font-bold text-(--clr-blue-950) md:text-(length:--fs-32)">
           Personal info
         </h2>
         <p className="text-(length:--fs-16) leading-(--lh-120) text-(--clr-grey-500)">
@@ -12,7 +12,7 @@ const PersonalInfo = () => {
         </p>
       </div>
 
-      <div className="flex flex-col gap-y-(--sp-300)">
+      <div className="flex flex-col md:gap-y-(--sp-300) lg:gap-y-(--sp-200)">
         <TextInput
           name="name"
           label="Name"
