88d317eb9e95871aafc7852d95d7141bd563a27b
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-06 16:59:46 +0100

    conditionally render Card component with yearlyPlans data array.

--- components/Forms/SelectPlan.tsx
+++ components/Forms/SelectPlan.tsx
@@ -6,6 +6,39 @@ import Card from "../Card";
 const SelectPlan = () => {
   const [isYearly, setIsYearly] = useState(false);
 
+  const yearlyPlans = [
+    {
+      plan: "arcade",
+      rate: "$90/yr",
+      bonus: "2 months free",
+    },
+    {
+      plan: "advanced",
+      rate: "$120/yr",
+      bonus: "2 months free",
+    },
+    {
+      plan: "pro",
+      rate: "$150/yr",
+      bonus: "2 months free",
+    },
+  ];
+
+  const monthlyPlans = [
+    {
+      plan: "arcade",
+      rate: "$9/mo",
+    },
+    {
+      plan: "advanced",
+      rate: "$12/mo",
+    },
+    {
+      plan: "pro",
+      rate: "$15/mo",
+    },
+  ];
+
   return (
     <div className="mx-(--sp-200) mb-[1.56rem] flex w-[21.4375rem] flex-col gap-y-(--sp-300) rounded-[0.625rem] bg-(--clr-white) px-(--sp-300) py-(--sp-400) shadow-[0_25px_40px_-20px_rgba(0,0,0,0.10)] md:m-0 md:w-full md:gap-y-(--sp-300) md:px-0 md:py-0 md:pt-[1.69rem] md:shadow-none lg:gap-y-(--sp-500)">
       <div className="flex flex-col gap-y-(--sp-100) md:mb-(--sp-100)">
@@ -17,9 +50,24 @@ const SelectPlan = () => {
         </p>
       </div>
       <div className="flex flex-col gap-y-(--sp-100) lg:flex-row lg:gap-x-(--sp-200) lg:gap-y-(--sp-300)">
-        <Card selected plan="arcade" rate="$9/mo" />
-        <Card plan="advanced" rate="$12/mo" />
-        <Card plan="pro" rate="$15/mo" />
+        {isYearly
+          ? yearlyPlans.map((plan) => (
+              <Card
+                key={plan.plan}
+                selected
+                plan={plan.plan}
+                rate={plan.rate}
+                bonus={plan.bonus}
+              />
+            ))
+          : monthlyPlans.map((plan) => (
+              <Card
+                key={plan.plan}
+                selected
+                plan={plan.plan}
+                rate={plan.rate}
+              />
+            ))}
       </div>
 
       <div className="flex w-full items-center justify-center rounded-[0.5rem] bg-(--clr-blue-50) py-[0.84375rem]">
