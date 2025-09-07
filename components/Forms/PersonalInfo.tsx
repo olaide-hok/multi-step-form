06297206537189f2c73d33fdd6548c057a59740d
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-07 17:17:54 +0100

    update component to receive props and pass them along to the respective TextInputs.

--- components/Forms/PersonalInfo.tsx
+++ components/Forms/PersonalInfo.tsx
@@ -1,6 +1,17 @@
+import { PersonalInfoFormDataType } from "@/app/page";
 import TextInput from "../TextInput";
 
-const PersonalInfo = () => {
+interface PersonalInfoProps {
+  inputValues: PersonalInfoFormDataType;
+  handleFormChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  errors: Partial<PersonalInfoFormDataType>;
+}
+
+const PersonalInfo = ({
+  inputValues,
+  errors,
+  handleFormChange,
+}: PersonalInfoProps) => {
   return (
     <div className="mx-(--sp-200) mb-[8.5rem] flex w-[21.4375rem] flex-col gap-y-(--sp-300) rounded-[0.625rem] bg-(--clr-white) px-(--sp-300) py-(--sp-400) shadow-[0_25px_40px_-20px_rgba(0,0,0,0.10)] md:m-0 md:w-full md:gap-y-(--sp-400) md:px-0 md:py-0 md:pt-[1.69rem] md:shadow-none lg:gap-y-(--sp-500)">
       <div className="flex flex-col gap-y-(--sp-100)">
@@ -18,18 +29,27 @@ const PersonalInfo = () => {
           label="Name"
           placeholder="e.g. Stephen King"
           type="text"
+          value={inputValues.name}
+          onChange={handleFormChange}
+          error={!!errors.name}
         />
         <TextInput
           name="email"
           type="email"
           label="Email Address"
           placeholder="e.g. stephenking@lorem.com"
+          value={inputValues.email}
+          onChange={handleFormChange}
+          error={!!errors.email}
         />
         <TextInput
-          name="phoneNumber"
+          name="phone"
           label="Phone Number"
           placeholder="e.g. +1234567890"
           type="text"
+          value={inputValues.phone}
+          onChange={handleFormChange}
+          error={!!errors.phone}
         />
       </div>
     </div>
