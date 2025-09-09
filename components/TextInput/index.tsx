628e1032f59e26947ad298166236d183c307144e
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-09 09:55:21 +0100

    update class typo error

--- components/TextInput/index.tsx
+++ components/TextInput/index.tsx
@@ -31,7 +31,7 @@ const TextInput = ({
       <input
         id={props.name}
         placeholder={placeholder}
-        className={`rounded-(--sp-100) border ${error ? "border-(--clr-red-500)" : "border-(--clr-purple-200)"} foucs:outline-(--clr-purple-600) h-[2.5rem] px-(--sp-200) py-(--sp-100) text-(length:--fs-16) leading-(--lh-120) font-medium text-(--clr-blue-950) placeholder:text-(--clr-grey-500) focus:outline-1 active:bg-white md:h-[3rem]`}
+        className={`rounded-(--sp-100) border ${error ? "border-(--clr-red-500)" : "border-(--clr-purple-200)"} h-[2.5rem] px-(--sp-200) py-(--sp-100) text-(length:--fs-16) leading-(--lh-120) font-medium text-(--clr-blue-950) placeholder:text-(--clr-grey-500) focus:outline-1 focus:outline-(--clr-purple-600) active:bg-white md:h-[3rem]`}
         {...props}
       />
     </div>
