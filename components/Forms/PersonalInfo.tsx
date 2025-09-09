f966058ad9c7fb816ef810a273f0468992d23f0f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-09 10:04:16 +0100

    change tag to h1

--- components/Forms/PersonalInfo.tsx
+++ components/Forms/PersonalInfo.tsx
@@ -15,9 +15,9 @@ const PersonalInfo = ({
   return (
     <div className="mx-(--sp-200) mb-[8.5rem] flex w-[21.4375rem] flex-col gap-y-(--sp-300) rounded-[0.625rem] bg-(--clr-white) px-(--sp-300) py-(--sp-400) shadow-[0_25px_40px_-20px_rgba(0,0,0,0.10)] md:m-0 md:w-full md:gap-y-(--sp-400) md:px-0 md:py-0 md:pt-[1.94rem] md:shadow-none lg:gap-y-(--sp-500)">
       <div className="flex flex-col gap-y-(--sp-100)">
-        <h2 className="text-(length:--fs-24) leading-(--lh-120) font-bold text-(--clr-blue-950) md:text-(length:--fs-32)">
+        <h1 className="text-(length:--fs-24) leading-(--lh-120) font-bold text-(--clr-blue-950) md:text-(length:--fs-32)">
           Personal info
-        </h2>
+        </h1>
         <p className="text-(length:--fs-16) leading-(--lh-120) text-(--clr-grey-500)">
           Please provide your name, email address, and phone number.
         </p>
